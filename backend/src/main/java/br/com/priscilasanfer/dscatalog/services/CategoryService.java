cc675e8fdac77957ecbbf3ac9769adc3c68f1851
Priscila Ferreira <priscilasanfer@gmail.com>
2021-08-17 13:12:59 -0300

    Add new Category with POST

--- backend/src/main/java/br/com/priscilasanfer/dscatalog/services/CategoryService.java
+++ backend/src/main/java/br/com/priscilasanfer/dscatalog/services/CategoryService.java
@@ -29,4 +29,12 @@ public class CategoryService {
                 .orElseThrow(() -> new EntityNotFoundException("Categoria com id: " + id + " não encontrada"));
         return new CategoryDTO(category);
     }
+
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category entity = new Category();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new CategoryDTO(entity);
+    }
 }
