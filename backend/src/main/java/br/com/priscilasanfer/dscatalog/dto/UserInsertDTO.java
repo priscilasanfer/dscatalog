349d2591c375af46b87153b2d24a99fd912c9cd7
Priscila Ferreira <priscilasanfer@gmail.com>
2021-08-25 18:09:59 -0300

    Custom ConstraintValidator

--- backend/src/main/java/br/com/priscilasanfer/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/br/com/priscilasanfer/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package br.com.priscilasanfer.dscatalog.dto;
 
+import br.com.priscilasanfer.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
     private String password;
 
---
+++ backend/src/main/java/br/com/priscilasanfer/dscatalog/services/validation/UserInsertValidator.java
@@ -0,0 +1,41 @@
+package br.com.priscilasanfer.dscatalog.services.validation;
+
+import br.com.priscilasanfer.dscatalog.controller.exceptions.FieldMessage;
+import br.com.priscilasanfer.dscatalog.dto.UserInsertDTO;
+import br.com.priscilasanfer.dscatalog.entities.User;
+import br.com.priscilasanfer.dscatalog.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void initialize(UserInsertValid ann) {
+    }
+
+    @Override
+    public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        User user = repository.findByEmail(dto.getEmail());
+
+        if (user != null) {
+            list.add(new FieldMessage("email", "Esse email já existe"));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+                    .addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
