61165aee5a4c40dcb706d4d9f00fbcc373878f0a
Priscila Ferreira <priscilasanfer@gmail.com>
2021-08-25 15:33:56 -0300

    Custom validation response

--- backend/src/main/java/br/com/priscilasanfer/dscatalog/controller/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/br/com/priscilasanfer/dscatalog/controller/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import br.com.priscilasanfer.dscatalog.services.exceptions.DatabaseException;
 import br.com.priscilasanfer.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -28,7 +30,7 @@ public class ResourceExceptionHandler {
 
     @ExceptionHandler(DatabaseException.class)
     public ResponseEntity<StandarError> databaseException(DatabaseException e,
-                                                                HttpServletRequest request) {
+                                                          HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         StandarError error = new StandarError();
         error.setTimestamp(Instant.now());
@@ -36,6 +38,23 @@ public class ResourceExceptionHandler {
         error.setError("Database exception");
         error.setMessage(e.getMessage());
         error.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e,
+                                                      HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError error = new ValidationError();
+        error.setTimestamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError("validation exception");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+
+        for (FieldError f : e.getBindingResult().getFieldErrors()) {
+            error.addError(f.getField(), f.getDefaultMessage());
+        }
         return ResponseEntity.status(status).body(error);
     }
 }
---
+++ backend/src/main/java/br/com/priscilasanfer/dscatalog/controller/exceptions/ValidationError.java
@@ -0,0 +1,16 @@
+package br.com.priscilasanfer.dscatalog.controller.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandarError{
+    private List<FieldMessage> erros = new ArrayList<>();
+
+    public List<FieldMessage> getErros() {
+        return erros;
+    }
+
+    public void addError(String fieldName, String message){
+        erros.add(new FieldMessage(fieldName, message));
+    }
+}
