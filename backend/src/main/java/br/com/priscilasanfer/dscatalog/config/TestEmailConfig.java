bcebcca479db6e4b1780b8e921474b014b9c82f3
Priscila Ferreira <priscilasanfer@gmail.com>
2021-09-19 08:20:05 -0300

    adicionando profile prod no mock de email

--- backend/src/main/java/br/com/priscilasanfer/dscatalog/config/TestEmailConfig.java
+++ backend/src/main/java/br/com/priscilasanfer/dscatalog/config/TestEmailConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("test")
+@Profile({"test","prod"})
 public class TestEmailConfig {
 
     @Bean
